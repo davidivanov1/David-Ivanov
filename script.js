f5ac5a1cd721488373f54a9819943c7bebb534ad
davidivanov1 <davidivanov047@gmail.com>
2024-12-01 19:05:26 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Write a JavaScript program that will calculate the price of 30 Phones, where the price of one phone is $119.95 and the tax rate is 5%.
+let num = 30;
+let price = 119.95;
+
+let sum = (num * price) * 1.05;
+console.log(sum);
+
+// Write a JavaScript program that takes minutes and converts them to seconds. Print the result in the console.
+
+let min = 15;
+let sec = min * 60;
+console.log(sec);
+
+
+// Write a JavaScript program that takes a number and increments the number by +1 and prints the result in the console.
+
+let num1 = 15;
+let sum1 = num1 + 1;
+console.log(sum1);
+
+// Write a JavaScript program that takes the base and height of a triangle and calculates its area. Print the result in the console.
+
+let c = 10;
+let h = 8;
+let area = (c * h) / 2;
+console.log(area);
+
+
+// Write a JavaScript program that takes length and width of a rectangle and finds its perimeter. Print the result in the console.
+
+let a = 6;
+let b = 4;
+let perimetar = 2 * a + 2 * b;
+console.log(perimetar);
+
+
+// Write a JavaScript program that will calculate area of circle. Print the result in the console.
+let r = 3;
+let pi = 3.14;
+let area1 = r * r * pi;
+console.log(area1);
